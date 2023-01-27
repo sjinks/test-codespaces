6336cb3b5a0f2ef5dcf85acfa9e89cda1e515601
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-01-27 14:36:58 +0200

    Simplify code

--- .devcontainer/.docker/php-fpm/setup.sh
+++ .devcontainer/.docker/php-fpm/setup.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-DOMAIN="localhost.localdomain"
-
-if [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" ] && [ -n "${CODESPACE_NAME}" ]; then
-    DOMAIN="${CODESPACE_NAME}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-fi
-
-if [ "$(id -u)" = "0" ]; then
-    su-exec www-data:www-data /dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
-    nginx
-    echo Application is listening on http://127.0.0.1:80
-else
-    /dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
-fi
+DOMAIN="${CODESPACE_NAME}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+su-exec www-data:www-data /dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
+nginx
+echo Application is listening on http://127.0.0.1:80
