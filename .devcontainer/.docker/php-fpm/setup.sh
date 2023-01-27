31118940666a5956df150a12a2bac3d6bd9d902f
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-01-27 17:53:16 +0200

    Fix

--- .devcontainer/.docker/php-fpm/setup.sh
+++ .devcontainer/.docker/php-fpm/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-DOMAIN="${CODESPACE_NAME}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-su-exec www-data:www-data /dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
+DOMAIN="${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+export WP_CLI_ALLOW_ROOT=1
+/dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
 nginx
-echo Application is listening on http://127.0.0.1:80
+echo Application is listening on http://127.0.0.1:8080
