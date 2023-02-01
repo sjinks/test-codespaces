ba437393d128e8193878d884d883b861db2bed91
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-01 16:41:48 +0200

    Get rid of dev-tools

--- .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-DOMAIN="${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+DOMAIN="${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 /usr/bin/mysqladmin -uroot password ''
 /dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "Test"
