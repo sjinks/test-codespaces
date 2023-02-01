96bc2b41b8b6688d7fe5a959cd94a3f384f2f8ce
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-01 15:23:29 +0200

    Bug fixes

--- .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
@@ -3,4 +3,5 @@
 set -x
 
 DOMAIN="${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+/usr/bin/mysqladmin -uroot password ''
 /dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "Test"
