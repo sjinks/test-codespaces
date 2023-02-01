369070087df30c9e28711d76ea8173cd16154794
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-01 14:36:42 +0200

    Merge in mariadb and memcached

--- .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
@@ -3,5 +3,4 @@
 set -x
 
 DOMAIN="${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-/dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
-echo Application is listening on http://127.0.0.1:8080
+/dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "Test"
