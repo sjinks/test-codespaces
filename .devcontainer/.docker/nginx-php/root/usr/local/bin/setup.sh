9a1395d9eaebdb93f66b7e9dcb3a1732f1e97997
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-03 02:30:11 +0200

    Final touch

--- .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
@@ -2,6 +2,10 @@
 
 set -x
 
-DOMAIN="${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-/usr/bin/mysqladmin -uroot password ''
-/dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "Test"
+if [ -n "${CODESPACE_NAME}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" ]; then
+    DOMAIN="${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+else
+    DOMAIN="localhost"
+fi
+
+exec /dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "WordPress VIP Development Site"
