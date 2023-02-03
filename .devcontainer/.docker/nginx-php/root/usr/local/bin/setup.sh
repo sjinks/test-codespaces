65512a06aff98f6bd0f0f9195a99362f6ee2748b
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-03 05:56:41 +0200

    Fix env var

--- .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/nginx-php/root/usr/local/bin/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -x
+env | sort
 
 if [ -n "${CODESPACE_NAME}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" ]; then
     DOMAIN="${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
@@ -10,9 +11,11 @@ fi
 
 /dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}/" "WordPress VIP Development Site"
 
-if [ -n "${CODESPACE_VSCODE_FOLDER}" ]; then
+if [ -n "${RepositoryName}" ]; then
     for i in client-mu-plugins images languages plugins themes; do
         rm -rf "/wp/wp-content/${i}"
-        ln -sf "${CODESPACE_VSCODE_FOLDER}/${i}" "/wp/wp-content/${i}"
+        ln -sf "/workspaces/${RepositoryName}/${i}" "/wp/wp-content/${i}"
     done
+    rm -rf /wp/vip-config
+    ln -sf "/workspaces/${RepositoryName}/vip-config" /wp/vip-config
 fi
