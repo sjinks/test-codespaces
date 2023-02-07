02259ddfde64eba4fe0328a7d748a3265489cf23
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-02-07 09:21:51 +0200

    Try bind mounts again

--- .devcontainer/.docker/single-container-app/root/usr/local/bin/setup.sh
+++ .devcontainer/.docker/single-container-app/root/usr/local/bin/setup.sh
@@ -12,13 +12,13 @@ else
     /dev-tools/setup.sh 127.0.0.1 root "http://${DOMAIN}" "WordPress VIP Development Site" "${DOMAIN}" "${WPVIP_MULTISITE_TYPE}"
 fi
 
-if [ -n "${RepositoryName}" ]; then
-    base=/workspaces/${RepositoryName}
-else
-    base=$(pwd)
-fi
+# if [ -n "${RepositoryName}" ]; then
+#     base=/workspaces/${RepositoryName}
+# else
+#     base=$(pwd)
+# fi
 
-for i in client-mu-plugins images languages plugins themes vip-config; do
-    sudo rm -rf "/wp/wp-content/${i}"
-    sudo ln -sf "${base}/${i}" "/wp/wp-content/${i}"
-done
+# for i in client-mu-plugins images languages plugins themes vip-config; do
+#     sudo rm -rf "/wp/wp-content/${i}"
+#     sudo ln -sf "${base}/${i}" "/wp/wp-content/${i}"
+# done
